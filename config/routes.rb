6d9ab27231e8bf3cac69e9f886cf047aac99300b
Michael Lee <mleenavy@gmail.com>
2017-06-04 18:21:35 -0700

    added ChatRooms, Messages, and added name to Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+  root 'chat_rooms#index', via: :get
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index", via: :get
 end
