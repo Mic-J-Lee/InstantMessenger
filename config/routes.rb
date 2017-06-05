dae45b989125a10e81b624830916a8cf40b03bb1
Michael Lee <mleenavy@gmail.com>
2017-06-04 20:58:19 -0700

    added redis, jquery, and actioncable

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :chat_rooms, only: [:new, :create, :show, :index]
   root 'chat_rooms#index', via: :get
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
